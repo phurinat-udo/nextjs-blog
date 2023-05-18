4158d4f99bb8d864e3a4eccdb8cbf32d67c7111b
phurinat-udo <phurinat.udo@outlook.com>
2023-05-19 00:47:22 +0700

    Update title

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home( {allPostsData} ) {
       <section className={utilStyles.headingMd}>
         <p> Graduate Student in Innovative Physics, <br/> Faculty of Science, Mahidol University </p>
         <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
+          (This is the website project for Application Development Course (SCIP514) - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
