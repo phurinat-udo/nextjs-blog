105a98fd181ae033a6a28b2b6c9e3cd7f632dcd8
phurinat-udo <phurinat.udo@outlook.com>
2023-05-18 15:56:37 +0700

    Update polished components

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,16 +1,23 @@
+import Date from "../../components/date";
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from '../../lib/posts';
-
+import Head from 'next/head';
+import utilStyles from '../../styles/utils.module.css';
 
 export default function Post({ postData }) {
     return (
         <Layout>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
-    </Layout>
+            <Head>
+                <title> {postData.title} </title>
+            </Head>
+            <article>
+                <h1 className= {utilStyles.handlingXl}> {postData.title} </h1>
+                <div className= {utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={ {__html: postData.contentHtml} }/>
+            </article>
+        </Layout>
     );
 }
 
@@ -22,7 +29,7 @@ export async function getStaticPaths() {
     };
 }
 export async function getStaticProps({ params }) {
-    const postData = getPostData(params.id);
+    const postData = await getPostData(params.id);
     return {
         props: {
         postData,
