a48ab72b997f1bb1a16fb435eeae41b503ba2221
phurinat-udo <phurinat.udo@outlook.com>
2023-05-18 21:18:38 +0700

    Update bio

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home( {allPostsData} ) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p> Graduate Student in Innovative Physics, <br/> Faculty of Science, Mahidol University </p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
