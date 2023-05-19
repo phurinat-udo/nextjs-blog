30e9046a416d162ddb8d2b0581d5428191001928
phurinat-udo <phurinat.udo@outlook.com>
2023-05-19 10:00:50 +0700

    try add style card

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home( {allPostsData} ) {
       <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={styles.grid}>
           {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
+            <li className={styles.card} key={id}>
               <Link href={`/posts/${id}`}>{title}</Link>
               <br />
               <small className={utilStyles.lightText}>
