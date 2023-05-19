c8096217ea3eb731a50054b34566d8912c31d666
phurinat-udo <phurinat.udo@outlook.com>
2023-05-19 09:56:54 +0700

    Test grid

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link';
 import Date from '../components/date';
+import styles from '../styles/Home.module.css';
 
 
 export default function Home( {allPostsData} ) {
@@ -22,7 +23,7 @@ export default function Home( {allPostsData} ) {
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
       <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
+        <ul className={styles.grid}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>{title}</Link>
